f9d1e96bc0119af913006be28959710dbb93a475
jflorez18 <ja.florezr1@uniandes.edu.co>
2026-05-09 20:00:23 -0500

    Se ajusta el taskdef.json para hacer prueba controlada para CD

--- tests/test_health.py
+++ tests/test_health.py
@@ -2,7 +2,7 @@ class TestHealthCheck:
 
     def test_health_check_returns_200(self, client):
         response = client.get("/")
-        assert response.status_code == 400
+        assert response.status_code == 200
 
     def test_health_check_returns_expected_body(self, client):
         data = client.get("/").get_json()
